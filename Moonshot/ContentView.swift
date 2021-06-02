2415c635427c6bbbc3e36ee93df853586a3b4bd8
johnjeang <johnjeang@gmail.com>
2021-06-01 19:05:03 -0500

    Add Geometry Reader

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      VStack{
+        GeometryReader { geo in
+          Image("example")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: geo.size.width)
+        }
+
+      }
     }
 }
 
